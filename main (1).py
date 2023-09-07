4eae28b149267a8e2a0b1fae76402e8d88cbde36
Vaithiswaran <144327542+vaithis2003@users.noreply.github.com>
2023-09-07 15:37:21 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+#1.1implement a recursive function to calculate the factorial of a given number. 
+
+def factorial(n):
+   if (n==1):
+      return 1
+   else:
+     return(n*factorial(n-1))
+num=4;
+print("number:",num)
+print("factorial:",factorial(num))
